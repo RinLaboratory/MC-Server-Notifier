99b3da021ee0a9fc6b0f332c6a2678830480a657
RinLaboratory <matiaschiongcampos@gmail.com>
2025-08-03 18:55:50 -0400

    refactor: updated server logic

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { FetchServer } from "./utils/fetch-server";
 import LoadYaml from "./utils/load-yaml";
 
 export default async function createApp() {
   const servers = await LoadYaml();
+  const interval = 15000; // 15 segundos
 
-  for (const server of servers) {
-    await FetchServer(server);
-  }
+  const task = async () => {
+    for (const server of servers) {
+      await FetchServer(server);
+    }
+  };
+
+  // Ejecutar la tarea inicialmente
+  await task();
+
+  // Configurar el intervalo para ejecutar la tarea repetidamente
+  setInterval(task, interval);
 }
