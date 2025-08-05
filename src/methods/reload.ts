53acc1678de1f41abdaa33f567c1a6a2b9def057
RinLaboratory <matiaschiongcampos@gmail.com>
2025-08-05 19:13:06 -0400

    refactor: added reload lang when reloading config

--- src/methods/reload.ts
+++ src/methods/reload.ts
@@ -1,10 +1,12 @@
 import { MessageFlags } from "discord.js";
 import type { BaseInteraction } from "discord.js";
 import loadConfigYaml from "~/utils/load-config-yaml";
+import loadLangYaml from "~/utils/load-lang-yaml";
 import logger from "~/utils/logger";
 
 export async function reload(interaction: BaseInteraction) {
   await loadConfigYaml();
+  await loadLangYaml();
 
   if (interaction.isRepliable()) {
     logger.log("reloaded config");
