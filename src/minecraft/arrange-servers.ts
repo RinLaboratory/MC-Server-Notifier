40435025ff32c0326d72f89d774414481aae2548
RinLaboratory <matiaschiongcampos@gmail.com>
2025-08-05 20:38:32 -0400

    refactor: simplified placeholder options

--- src/minecraft/arrange-servers.ts
+++ src/minecraft/arrange-servers.ts
@@ -39,98 +39,35 @@ export async function arrangeServers({
         (value) => value.serverName === server.name,
       );
 
+      const placeholder_options = {
+        server_name: server.name,
+        server_status: ServerInfo,
+        server_runtime: response.version?.name ?? "",
+        server_url: server.config.serverURL,
+        server_ip: server.config.serverIP,
+        server_port: server.config.serverPort,
+        version_protocol: response.version?.protocol ?? 0,
+        player_count: response.players?.online ?? 0,
+        max_player_count: response.players?.max ?? 0,
+        last_available: lastMentionedServer
+          ? `<t:${lastMentionedServer.timestamp}:R>`
+          : t("server.last-fetch.never"),
+      };
+
       arrangedServers.push([
         {
-          name: t("server.first-column.top", {
-            server_name: server.name,
-            server_status: ServerInfo,
-            server_runtime: response.version?.name ?? "",
-            server_url: server.config.serverURL,
-            server_ip: server.config.serverIP,
-            server_port: server.config.serverPort,
-            version_protocol: response.version?.protocol ?? 0,
-            player_count: response.players?.online ?? 0,
-            max_player_count: response.players?.max ?? 0,
-            last_available: lastMentionedServer
-              ? `<t:${lastMentionedServer.timestamp}:R>`
-              : t("server.last-fetch.never"),
-          }),
-          value: t("server.first-column.bottom", {
-            server_name: server.name,
-            server_status: ServerInfo,
-            server_runtime: response.version?.name ?? "",
-            server_url: server.config.serverURL,
-            server_ip: server.config.serverIP,
-            server_port: server.config.serverPort,
-            version_protocol: response.version?.protocol ?? 0,
-            player_count: response.players?.online ?? 0,
-            max_player_count: response.players?.max ?? 0,
-            last_available: lastMentionedServer
-              ? `<t:${lastMentionedServer.timestamp}:R>`
-              : t("server.last-fetch.never"),
-          }),
+          name: t("server.first-column.top", placeholder_options),
+          value: t("server.first-column.bottom", placeholder_options),
           inline: true,
         },
         {
-          name: t("server.middle-column.top", {
-            server_name: server.name,
-            server_status: ServerInfo,
-            server_runtime: response.version?.name ?? "",
-            server_url: server.config.serverURL,
-            server_ip: server.config.serverIP,
-            server_port: server.config.serverPort,
-            version_protocol: response.version?.protocol ?? 0,
-            player_count: response.players?.online ?? 0,
-            max_player_count: response.players?.max ?? 0,
-            last_available: lastMentionedServer
-              ? `<t:${lastMentionedServer.timestamp}:R>`
-              : t("server.last-fetch.never"),
-          }),
-          value: t("server.middle-column.bottom", {
-            server_name: server.name,
-            server_status: ServerInfo,
-            server_runtime: response.version?.name ?? "",
-            server_url: server.config.serverURL,
-            server_ip: server.config.serverIP,
-            server_port: server.config.serverPort,
-            version_protocol: response.version?.protocol ?? 0,
-            player_count: response.players?.online ?? 0,
-            max_player_count: response.players?.max ?? 0,
-            last_available: lastMentionedServer
-              ? `<t:${lastMentionedServer.timestamp}:R>`
-              : t("server.last-fetch.never"),
-          }),
+          name: t("server.middle-column.top", placeholder_options),
+          value: t("server.middle-column.bottom", placeholder_options),
           inline: true,
         },
         {
-          name: t("server.last-column.top", {
-            server_name: server.name,
-            server_status: ServerInfo,
-            server_runtime: response.version?.name ?? "",
-            server_url: server.config.serverURL,
-            server_ip: server.config.serverIP,
-            server_port: server.config.serverPort,
-            version_protocol: response.version?.protocol ?? 0,
-            player_count: response.players?.online ?? 0,
-            max_player_count: response.players?.max ?? 0,
-            last_available: lastMentionedServer
-              ? `<t:${lastMentionedServer.timestamp}:R>`
-              : t("server.last-fetch.never"),
-          }),
-          value: t("server.last-column.bottom", {
-            server_name: server.name,
-            server_status: ServerInfo,
-            server_runtime: response.version?.name ?? "",
-            server_url: server.config.serverURL,
-            server_ip: server.config.serverIP,
-            server_port: server.config.serverPort,
-            version_protocol: response.version?.protocol ?? 0,
-            player_count: response.players?.online ?? 0,
-            max_player_count: response.players?.max ?? 0,
-            last_available: lastMentionedServer
-              ? `<t:${lastMentionedServer.timestamp}:R>`
-              : t("server.last-fetch.never"),
-          }),
+          name: t("server.last-column.top", placeholder_options),
+          value: t("server.last-column.bottom", placeholder_options),
           inline: true,
         },
       ]);
