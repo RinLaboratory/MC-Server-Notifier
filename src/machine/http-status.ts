70112b0c6eebb87e9c506fed730dd152171829a6
RinLaboratory <matiaschiongcampos@gmail.com>
2025-08-27 15:19:22 -0400

    feat: implemented http requests

---
+++ src/machine/http-status.ts
@@ -0,0 +1,21 @@
+import type { TServer } from "~/utils/validators";
+import * as http from "~/utils/http";
+import type { JavaStatusLegacyResponse } from "minecraft-server-util";
+
+export async function httpStatus(
+  server: TServer,
+): Promise<Partial<JavaStatusLegacyResponse>> {
+  try {
+    const response = await http.get<{ uptime: number }>(
+      `http://${server.config.serverIP}:${server.config.serverPort}/healthcheck`,
+    );
+
+    return {
+      version: { name: `Online active ${response.uptime}`, protocol: 0 },
+    };
+  } catch {
+    return {
+      version: { name: "Unknown - Offline", protocol: 0 },
+    };
+  }
+}
--- src/minecraft/arrange-servers.ts
+++ src/minecraft/arrange-servers.ts
@@ -7,6 +7,7 @@ import { clientStore } from "~/store/client-store";
 import logger from "~/utils/logger";
 import type { APIEmbedField } from "discord.js";
 import { t } from "~/utils/translations";
+import { httpStatus } from "~/machine/http-status";
 
 interface UpdateArrangementProps {
   serverResponse: TServerResponse[];
@@ -23,11 +24,58 @@ export async function arrangeServers({
   }
 
   for (const { server, response } of serverResponse) {
-    if (server.isMachine) {
+    if (server.type === "machine") {
       const ServerInfo = await getMachineStatus({
         server,
       });
       arrangedServers.push(ServerInfo);
+    } else if (server.type === "http") {
+      const httpServerResponse = await httpStatus(server);
+      const ServerInfo = await serverStatus({
+        server,
+        serverResponse: httpServerResponse,
+      });
+
+      const { memorizedLastMentionTimestamp, hideServersURLs } =
+        messageStore.getState();
+      const lastMentionedServer = memorizedLastMentionTimestamp.find(
+        (value) => value.serverName === server.name,
+      );
+
+      const placeholder_options = {
+        server_name: server.name,
+        server_status: ServerInfo,
+        server_runtime: response.version?.name ?? "",
+        server_url: hideServersURLs
+          ? t("commands.toggle-servers-urls.hidden")
+          : server.config.serverURL,
+        server_ip: server.config.serverIP,
+        server_port: server.config.serverPort,
+        version_protocol: response.version?.protocol ?? 0,
+        player_count: response.players?.online ?? 0,
+        max_player_count: response.players?.max ?? 0,
+        last_available: lastMentionedServer
+          ? `<t:${lastMentionedServer.timestamp}:R>`
+          : t("server.last-fetch.never"),
+      };
+
+      arrangedServers.push([
+        {
+          name: t("server.first-column.top", placeholder_options),
+          value: t("server.first-column.bottom", placeholder_options),
+          inline: true,
+        },
+        {
+          name: t("server.middle-column.top", placeholder_options),
+          value: t("server.middle-column.bottom", placeholder_options),
+          inline: true,
+        },
+        {
+          name: t("server.last-column.top", placeholder_options),
+          value: t("server.last-column.bottom", placeholder_options),
+          inline: true,
+        },
+      ]);
     } else {
       const ServerInfo = await serverStatus({
         server,
